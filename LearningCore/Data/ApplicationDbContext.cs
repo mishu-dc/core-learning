74e361354626cd1a9a84d5ec002b38b0c82e1d4d
faisal ahmed <mishu_dc@yahoo.com>
2019-05-23 00:26:27 -0400

    Initial Commit

---
+++ LearningCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using LearningCore.Data.Configurations;
+using LearningCore.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Profile> Profile { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //builder.ApplyConfiguration(new ProfileConfiguration());
+            //builder.ApplyConfiguration(new ProjectConfiguration());
+            builder.ApplyConfigurationsFromAssembly(assembly: Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ LearningCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearningCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
